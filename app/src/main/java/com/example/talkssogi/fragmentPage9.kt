c47ddd0fdb1a0349948a41f1a4d93db5d06abcb6
kjh <kangji@naver.com>
2024-07-10 02:49:40 +0900

    page9 searching part

--- app/src/main/java/com/example/talkssogi/fragmentPage9.kt
+++ app/src/main/java/com/example/talkssogi/fragmentPage9.kt
@@ -5,11 +5,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.Spinner
 import androidx.fragment.app.Fragment
 import java.util.Calendar
 
 class fragmentPage9 : Fragment() {
+    // private lateinit var viewModel: MyViewModel // 스피너의 항목을 채팅방 구성원 이름 가져와서 출력
+    private var selectedDate1: String? = null // 시작할 날짜 저장
+    private var selectedDate2: String? = null // 끝날 날짜 저장
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -17,11 +23,29 @@ class fragmentPage9 : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_page9, container, false)
 
-        val buttonOpenCalendar = view.findViewById<Button>(R.id.button_open_calendar)
+        val buttonOpenCalendar1 = view.findViewById<Button>(R.id.button_open_calendar1)
+        val buttonOpenCalendar2 = view.findViewById<Button>(R.id.button_open_calendar2)
+        val searchSpinner: Spinner = view.findViewById<Spinner>(R.id.search_spinner)
+        val resultsSpinner: Spinner = view.findViewById<Spinner>(R.id.results_spinner)
 
-        buttonOpenCalendar.setOnClickListener {
-            showDatePicker(buttonOpenCalendar)
+        buttonOpenCalendar1.setOnClickListener {
+            showDatePicker(buttonOpenCalendar1)
         }
+
+        buttonOpenCalendar2.setOnClickListener {
+            showDatePicker(buttonOpenCalendar2)
+        }
+
+        val searchItems = arrayOf("전체", "1", "2")
+        // 뷰모델에서 대화 참여자 이름 가져와서 저장
+        val searchAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, searchItems)
+        searchAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        searchSpinner.adapter = searchAdapter
+
+        val resultsItems = arrayOf("보낸 메시지 수 그래프", "활발한 시간대 그래프", "대화를 보내지 않은 날짜")
+        val resultsAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, resultsItems)
+        resultsAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        resultsSpinner.adapter = resultsAdapter
 
         return view
     }
@@ -34,10 +58,19 @@ class fragmentPage9 : Fragment() {
 
         val datePickerDialog = DatePickerDialog(
             requireContext(),
-            { _, year, month, day ->
-                val selectedDate = "${year}년 ${month + 1}월 ${day}일"
-                // 버튼 텍스트 변경
-                button.text = selectedDate
+            { _, selectedYear, selectedMonth, selectedDay ->
+                val selectedDate = "${selectedYear}년 ${selectedMonth + 1}월 ${selectedDay}일"
+
+                when (button.id) {
+                    R.id.button_open_calendar1 -> {
+                        selectedDate1 = selectedDate // 선택된 날짜 저장
+                        button.text = selectedDate1
+                    }
+                    R.id.button_open_calendar2 -> {
+                        selectedDate2 = selectedDate // 선택된 날짜 저장
+                        button.text = selectedDate2
+                    }
+                }
             },
             year,
             month,
