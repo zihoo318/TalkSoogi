2fd5cef81d12c2211737b5c57869c9a5ed357351
kjh <kangji@naver.com>
2024-07-17 23:07:38 +0900

    page3 connect

--- TalkSsogi_Android/app/src/main/java/com/example/talkssogi/ApiService.kt
+++ TalkSsogi_Android/app/src/main/java/com/example/talkssogi/ApiService.kt
@@ -4,14 +4,16 @@ import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface ApiService {
 
     @GET("/api/userIds") //페이지1에서 쓸 유저 아이디 목록
     fun getAllUserIds(): Call<UserIdResponse>
+
     @GET("/api/chatrooms") //채팅방 목록
     fun getChatRooms(
-        @Header("Authorization") authToken: String  //Authorization 헤더에 authToken이라는 이름으로 세션 ID 전달
+        @Query("ID") userID: String?
     ): Call<List<ChatRoom>>
 
 }
