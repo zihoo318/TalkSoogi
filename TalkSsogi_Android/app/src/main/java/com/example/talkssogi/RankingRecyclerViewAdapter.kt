5053efe26a20a03310798046fb9ab61c6d38ee0a
Yujinne <2291011@hansung.ac.kr>
2024-08-25 16:53:03 +0900

    코드 올리기

--- TalkSsogi_Android/app/src/main/java/com/example/talkssogi/RankingRecyclerViewAdapter.kt
+++ TalkSsogi_Android/app/src/main/java/com/example/talkssogi/RankingRecyclerViewAdapter.kt
@@ -1,4 +1,40 @@
 package com.example.talkssogi
 
-class RankingRecyclerViewAdapter {
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class RankingRecyclerViewAdapter(private var rankingList: List<String>) : RecyclerView.Adapter<RankingRecyclerViewAdapter.RankingViewHolder>() {
+
+    inner class RankingViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val rankingResut: TextView = itemView.findViewById(R.id.ranking_result)
+
+
+        fun bind(ranking: String) {
+            rankingResut.text = ranking
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RankingViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_ranking, parent, false)
+        return RankingViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: RankingViewHolder, position: Int) {
+        try {
+            holder.bind(rankingList[position])
+        } catch (e: Exception) {
+            Log.e("RecyclerView", "Error in onBindViewHolder: ${e.message}")
+        }
+    }
+
+    override fun getItemCount(): Int = rankingList.size
+
+    fun updateData(newItems: List<String>) {
+        this.rankingList = newItems ?: emptyList()  // Null-safe 처리
+        notifyDataSetChanged()
+    }
 }
